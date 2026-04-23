dff5226feff3a298360cbe69cfbf78e4274f8fcf
Jonas Eduardo C. Martins <106180613+JonasEduard0@users.noreply.github.com>
2026-04-23 11:49:35 -0300

    Delete poojava directory

--- poojava/Formato.java
+++
@@ -1,8 +0,0 @@
-package poojava;
-
-public abstract class Formato {
-    abstract double area();//abstrato é criado no pai abstrato, implementado no filho
-    private final int abc = 0;//final nn permite a variavel ser reescrita
-    
-    void display(){ //Esses métodos são concretos, sao implementados no pai
-        System.out.println("Este e um formato.");}}
